68d5c9ed357faf4890f2b2ec1b8b3a7112f9c663
Gururaj2003 <149136721+Gururaj2003@users.noreply.github.com>
2026-01-17 11:37:12 +0530

    .

--- core/settings.py
+++ core/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = ['15.206.212.48', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
